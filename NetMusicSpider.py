cc939fb6392b82fe54717d1e673063736171be3b
Zunler <1024047176@qq.com>
2020-03-06 12:29:34 +0800

    Refact

--- NetMusicSpider.py
+++ NetMusicSpider.py
@@ -5,24 +5,26 @@ import json
 
 
 class NetMusicSpider(object):
+    chinese: str = "https://music.163.com/discover/artist/cat?id=100"
+    europe: str = "https://music.163.com/discover/artist/cat?id=200"
+    japan: str = "https://music.163.com/discover/artist/cat?id=600"
+    korea: str = "https://music.163.com/discover/artist/cat?id=700"
+    headers: dict = {"Accept": "application/json, text/plain, */*",
+                     "Accept-Encoding": "gzip, deflate",
+                     "Accept-Language": "zh-HK,zh;q=0.9,en-HK;q=0.8,en;q=0.7,zh-CN;q=0.6,zh-TW;q=0.5,en-US;q=0.4",
+                     "Content-Type": "application/json;charset=UTF-8",
+                     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"}
+
     def __init__(self):
-        self.headers = headers = {"Accept": "application/json, text/plain, */*",
-                                  "Accept-Encoding": "gzip, deflate",
-                                  "Accept-Language": "zh-HK,zh;q=0.9,en-HK;q=0.8,en;q=0.7,zh-CN;q=0.6,zh-TW;q=0.5,en-US;q=0.4",
-                                  "Content-Type": "application/json;charset=UTF-8",
-                                  "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"}
+
         self.session = requests.session()
         self.sqlData = []
-        self.chinese = "https://music.163.com/discover/artist/cat?id=100"
-        self.europe = "https://music.163.com/discover/artist/cat?id=200"
-        self.japan = "https://music.163.com/discover/artist/cat?id=600"
-        self.korea = "https://music.163.com/discover/artist/cat?id=700"
 
     def write_to_mysql(self, host, user, password, db, sql, port, charset):
-        #创建连接
+        # 创建连接
         conn = connect(host=host, port=port, db=db, user=user, password=password,
                        charset=charset)
-        #游标
+        # 游标
         cur = conn.cursor()
         print("start to write to database.......")
         for item in self.sqlData:
@@ -36,9 +38,17 @@ class NetMusicSpider(object):
         conn.close()
         print('write to database success!')
 
-    def getHotMusic(self):
-        #获取热门音乐
-        print("start to get hot music ")
+    def transform_music_length(self, time_stamp):
+        min = int(time_stamp / 1000 / 60)
+        second = int(time_stamp / 1000 % 60)
+        if (min < 10):
+            min = "0" + str(min)
+        if (second < 10):
+            second = "0" + str(second)
+        return str(min) + ":" + str(second)
+
+    def get_hot_music(self):
+        # 获取热门音乐
         url = "https://music.163.com/discover/toplist?id=3778678"
         re = self.session.get("https://music.163.com/discover/toplist?id=3778678", headers=self.headers)
         soup = BeautifulSoup(re.text, "lxml")
@@ -53,56 +63,54 @@ class NetMusicSpider(object):
             artist_name = item['artists'][0]['name']
             artist_id = item['artists'][0]['id']
             pic_url = item['album']['picUrl']
-            timeStamp =item['duration']
-            min=int(timeStamp/1000/60)
-            second=int(timeStamp/1000%60)
-            music_length="0"+str(min)+":"+str(second)
-            print(music_length)
-            self.sqlData.append([music_id,music_name,music_length,artist_id, artist_name, pic_url, source_path])
-            print([music_id,music_name,music_length,artist_id, artist_name, pic_url, source_path])
-        print("fetch all music success")
+            timeStamp = item['duration']
+            music_length = self.transform_music_length(timeStamp)
+            self.sqlData.append([music_id, music_name, music_length, artist_id, artist_name, pic_url, source_path])
+            print([music_id, music_name, music_length, artist_id, artist_name, pic_url, source_path])
 
-    def getHotArtist(self, type):
+    print("fetch all music success")
 
-        #获取某类型的热门歌手
-        if (type == 'chinese'):
-            url = self.chinese
-        elif (type == 'europe'):
-            url = self.europe
-        elif (type == 'japan'):
-            url = self.japan
-        elif (type == 'korea'):
-            url = self.korea
-        print("start to get hot artist...... ")
-        for i in range(1, 4):
-            req = self.session.get(url + str(i), headers=self.headers)
-            soup = BeautifulSoup(req.text, "lxml")
-            cover = soup.find_all("div", class_="u-cover u-cover-5")
-            artistDetail = soup.find_all("li", class_="sml")
-            # 封面歌手,直接含有封面
-            for item in cover:
-                src = item.find('img')['src']
-                a = item.find('a')
-                artist_id = str(a['href']).split("=")[1]
-                artist_name = a['title'][:-3]
-                self.sqlData.append([artist_id, artist_name, src])
-                print([artist_id, artist_name, src])
-            # 列表歌手，需要进入主页找封面
-            for item in artistDetail:
-                a = item.find('a', class_="nm nm-icn f-thide s-fc0")
-                artist_id = str(a['href']).split("=")[1]
-                artist_name = a.string
-                home_url = "https://music.163.com/artist?id=" + artist_id
-                home_req = self.session.get(home_url, headers=self.headers)
-                home_soup = BeautifulSoup(home_req.text, 'lxml')
-                script = json.loads(home_soup.find("script", type="application/ld+json").string)
-                src = script['images'][0]
-                self.sqlData.append([artist_id, artist_name, src])
-                print([artist_id, artist_name, src])
-            print("fetch all artist success!")
+
+def get_hot_artist(self, type):
+    # 获取某类型的热门歌手
+    if (type == 'chinese'):
+        url = self.chinese
+    elif (type == 'europe'):
+        url = self.europe
+    elif (type == 'japan'):
+        url = self.japan
+    elif (type == 'korea'):
+        url = self.korea
+    print("start to get hot artist...... ")
+    for i in range(1, 4):
+        req = self.session.get(url + str(i), headers=self.headers)
+        soup = BeautifulSoup(req.text, "lxml")
+        cover = soup.find_all("div", class_="u-cover u-cover-5")
+        artistDetail = soup.find_all("li", class_="sml")
+        # 封面歌手,直接含有封面
+        for item in cover:
+            src = item.find('img')['src']
+            a = item.find('a')
+            artist_id = str(a['href']).split("=")[1]
+            artist_name = a['title'][:-3]
+            self.sqlData.append([artist_id, artist_name, src])
+            print([artist_id, artist_name, src])
+        # 列表歌手，需要进入主页找封面
+        for item in artistDetail:
+            a = item.find('a', class_="nm nm-icn f-thide s-fc0")
+            artist_id = str(a['href']).split("=")[1]
+            artist_name = a.string
+            home_url = "https://music.163.com/artist?id=" + artist_id
+            home_req = self.session.get(home_url, headers=self.headers)
+            home_soup = BeautifulSoup(home_req.text, 'lxml')
+            script = json.loads(home_soup.find("script", type="application/ld+json").string)
+            src = script['images'][0]
+            self.sqlData.append([artist_id, artist_name, src])
+            print([artist_id, artist_name, src])
+        print("fetch all artist success!")
 
 
 if __name__ == "__main__":
     NMS = NetMusicSpider()
-    NMS.getHotMusic()
-    NMS.write_to_mysql("172.16.29.107","hive","NEU@pzj123456","music","INSERT INTO TB_SYS_MUSIC_INFO VALUES (%s,%s,%s,%s,%s,%s,%s)",3306,"utf8")
+    NMS.get_hot_music()
+    NMS.write_to_mysql("172.16.29.107", "user", "pass", "music","INSERT INTO TB_SYS_MUSIC_INFO VALUES (%s,%s,%s,%s,%s,%s,%s)", 3306, "utf8")
--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 ## 安装包
 1. bs4
 2. pymysql
-## getHotMusic
+## get_hot_music
 获取热门音乐排行榜所有音乐
-## getHotArtist(type)
+## get_hot_artist(type)
 获取热门类型所有歌手
-type chinese japan korea europe
+
+type 
+chinese japan korea europe
 ## write_to_mysql
 写到数据库
